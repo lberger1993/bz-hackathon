c18f4339fbaed1367ba916d5427efc95b7c7c820
lberger1993 <berger.luciaeve@gmail.com>
2017-11-10 19:47:05 +0100

    Adds intial routing

--- flask-mw/app.py
+++ flask-mw/app.py
@@ -1,32 +1,45 @@
-from flask import Flask, send_file
-from flask_restful import Resource, Api
+import sqlite3
+import json
+
+from flask import Flask, send_file, request
 
 app = Flask(__name__)
-api = Api(app)
 
-#
-# class HelloWorld(Resource):
-#     def get(self):
-#         return {'hello': 'world'}
+conn = sqlite3.connect('../data/WaterFP.sqlite', check_same_thread=False)
+cur = conn.cursor()
 
-#
-# class GetInitalRecipes(Resource):
-#     def get(self):
-#         # return
-#         {recipe_list: [{
-#                 recipe_name:
-#
-#         }]}
 
 @app.route("/")
 def index():
     return send_file("templates/index.html")
 
 
+@app.route("/api/v1/get_all_food", methods=['GET'])
+def return_all_foods():
+    db_data = cur.execute("SELECT * FROM FoodItems").fetchall()
+    product_list = []
+    for prod in db_data:
+        json_item = json.loads('{"id" : %d, "ProductName" : "%s", "WaterCost" : %s}' % (prod[0], prod[1], prod[2]))
+        product_list.append(json_item)
+    return json.dumps(product_list)
 
 
+@app.route("/api/v1/get_all_recipes", methods=['GET'])
+def return_all_recipes():
+    db_data = cur.execute("SELECT * FROM Recipies").fetchall()
+    product_list = []
+    for prod in db_data:
+        json_item = json.loads('{"id" : %d, "RecipieName" : "%s"}' % (prod[0], prod[1]))
+        product_list.append(json_item)
+    return json.dumps(product_list)
 
-# api.add_resource(HelloWorld, '/hello')
+
+@app.route("/api/v1/calculate_water_score", methods=["POST"])
+def calculate_water_score():
+    print(request.data)
+
+
+# def reedem_points_for_local_purchases():
 
 if __name__ == '__main__':
     app.run(debug=True)
