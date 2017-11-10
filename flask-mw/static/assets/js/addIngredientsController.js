4aacfd43cba172da21170a605e7f6ef573c6b5fd
Hlib Babii <hlib.babii@unibz.it>
2017-11-10 21:41:44 +0100

    add call to the backend

--- flask-mw/static/assets/js/addIngredientsController.js
+++ flask-mw/static/assets/js/addIngredientsController.js
@@ -1,9 +1,22 @@
 angular.module('HelloWorldApp', [])
-    .controller('HelloWorldController', function($scope) {
+    .controller('HelloWorldController', ['$scope', '$http', function($scope, $http) {
         $scope.ingredients = [];
         $scope.newIngredient = "";
+        $scope.foodList = "";
+        //TODO remove this from HelloWorld to smth better
 
+        //TODO move this somewhere:
         $scope.addIngredient = function() {
             $scope.ingredients.push($scope.newIngredient);
+
+            $http({
+                method: 'GET',
+                url: '/api/v1/get_all_food'
+            }).then(function successCallback(response) {
+                $scope.foodList = response.data;
+            }, function errorCallback(response) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+            });
         }
-    });
+    }]);
