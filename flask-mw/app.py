8bfcbd139a5a0e5ccc0565bfe24dcb3eb7138ec6
lberger1993 <berger.luciaeve@gmail.com>
2017-11-10 15:54:15 +0100

    Adds templating

--- flask-mw/app.py
+++ flask-mw/app.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, send_file
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
+
 class HelloWorld(Resource):
     def get(self):
+        # actaully from database
         return {'hello': 'world'}
+
+@app.route("/")
+def index():
+    return send_file("templates/index.html")
 
 api.add_resource(HelloWorld, '/')
 
