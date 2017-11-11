0a7b768653a9eeb30867f30befd12d54f1e65959
Srishti71 <srishti.kristi12@gmail.com>
2017-11-11 06:44:42 +0100

    "Commit"

--- flask-mw/static/assets/js/chooseRecipeController.js
+++ flask-mw/static/assets/js/chooseRecipeController.js
@@ -4,7 +4,9 @@ angular.module('chooseRecipeApp', [])
          {imageName : 'spaghetti', imageSrc : '../assets/img/spaghetti-bolognese.jpeg' },
         {imageName : 'pasta', imageSrc : '../assets/img/pasta.jpeg' },
         ];
-        console.log('and error here?')
+        console.log('and error here?');
+
+       $scope.checkValue ="hi";
 
         $http({
             method: 'GET',
@@ -18,6 +20,42 @@ angular.module('chooseRecipeApp', [])
            $scope.selected_recipe = 'pizza';
            $scope.optimizeOption = function(){
 
+           };
+
+           $scope.optimal= false;
+
+           $scope.optimize = function () {
+
+               optimal = true;
+               matrix = [{ProductName: 'Beef', OptimizedProductName: 'Mushroom', WaterPerKilo: 150, AmountInRecipe : 0.5}];
+
+
+
            }
+/* $scope.matrix;
+          $http({
+           method: 'GET',
+           url: '/api/v1/get_recipe_partners'
+       }).then(function successCallback(response) {
+            $scope.matrix = response.data;
+
+       }, function errorCallback(response) {
+           //TODO add error processing
+       });
+          $scope.selected_recipe = 'pizza';
+          $scope.optimizeOption = function(recipe_id){
+          for (var row in $scope.matrix){
+                  $scope.matrix[row]['id']
+                  if ($scope.matrix[row]['id'] == recipe_id){
+                       var optimized_recipeID = $scope.matrix[row]['good_recipe']
+                       for (var option in $scope.Foodrecipes){
+                           if (optimized_recipeID == $scope.Foodrecipes[option]['RecipeID'])
+                               $scope.best_option =$scope.Foodrecipes[option];
+                               console.log('my best choice', $scope.best_option);
+                           }
+                       }
+               }
+          }*/
+
 
 }]);
